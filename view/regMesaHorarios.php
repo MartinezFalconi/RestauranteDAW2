9c8874dc27974c68d95f2713403b3abc77fe73e7
MartinezFalconi <sergiomafa09@gmail.com>
2020-11-03 19:30:08 +0100

    test

--- view/regMesaHorarios.php
+++ view/regMesaHorarios.php
@@ -1,16 +1,25 @@
-<?php
-//MartinezFalconi
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Registros | Restaurante</title>
+    </head>
 
+    <body>
+
+    <?php
+    echo "<a class='atras' href='./zonaRestaurante.php?espacio=Terraza'>Atrás</a>";
+
+    require_once '../controller/sessionController.php';
     include_once '../model/mesaDAO.php';
-    
-    echo "<table>";
+    echo "<table id='table' class='viewMesas'>";
     $mesaDAO = new MesaDAO();
     echo $mesaDAO->viewMesas();
     echo "</table>";
-    
 
-//JaramilloVives
-
-//FernandezVives
-
-?>
+    ?>
+        
+    </body>
+</html>
